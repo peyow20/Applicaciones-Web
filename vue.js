ee29dc590d6a0bc2ba7d605a0a96c5855703f3f4
peyow20 <a00825988@itesm.mx>
2022-03-16 14:34:07 -0600

    Practica 4

---
+++ vue.js
@@ -0,0 +1,11 @@
+Vue.createApp({
+  data() {
+    return {
+      information: {
+        nombre: "Alfredo Jose Welsh Martinez",
+        matricula: "A00825988"
+      },
+      Image: "https://agustimascotas320.webnode.es/_files/system_preview_detail_200000005-5a1755c08f-public/Im%C3%A1genes-de-Perritos-Jugando-16.jpg"
+    };
+  }
+}).mount("#app");
